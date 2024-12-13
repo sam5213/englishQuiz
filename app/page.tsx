093329e7a6c8cf631bca784eb9d70f4eb0afeb12
sam5213 <gulmirazhb@gmail.com>
2024-12-13 16:38:19 +0500

    Update url of back in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -171,7 +171,7 @@ export default function EnglishQuiz() {
     if (selectedDate && selectedTime) {
       console.log(`Booking confirmed for ${selectedDate.toDateString()} at ${selectedTime}`)
 
-      await fetch('/api/sendToTelegram', {
+      await fetch('https://082d-178-176-83-2.ngrok-free.app/post', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -192,7 +192,7 @@ export default function EnglishQuiz() {
       .map(([category, answers]) => `${category}: ${answers.join(', ')}`)
       .join('\n')
 
-    await fetch('/api/sendToTelegram', {
+    await fetch('https://082d-178-176-83-2.ngrok-free.app/post', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
