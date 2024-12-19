74a902cdc428dd2f1ddcb8fd54b8925e0a01896c
sam5213 <gulmirazhb@gmail.com>
2024-12-19 23:20:51 +0500

    remove toDateString from SelectedTime

--- script.js
+++ script.js
@@ -198,7 +198,7 @@ async function confirmBooking() {
         });
         
         try {
-            tg.sendData('Бесплатный урок забронирован на: ' + state.selectedDate.toDateString() + ' ' + state.selectedTime.toTimeString());
+            tg.sendData('Бесплатный урок забронирован на: ' + state.selectedDate.toDateString() + ' ' + state.selectedTime);
             tg.sendData(GROUP_ID, resultSummary);
             const response = await fetch(`${BACKEND_URL}/api/sendToTelegram`, {
                 method: 'POST',
